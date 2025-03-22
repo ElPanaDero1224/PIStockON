bb2ab9b0b753949b808f37fe099f317a8ac271bf
ElPanaDero1224 <samelibb10@gmail.com>
2025-03-22 14:24:06 -0600

    proveedores

--- StockON/resources/views/proveedores.blade.php
+++ StockON/resources/views/proveedores.blade.php
@@ -1,5 +1,39 @@
 @extends('plantillas.cabeza2')
 
+@section('css')
+    @vite('resources/css/proveedores.css')
+@endsection
+
+@section('contenido')
+
+<div style="text-align: center; margin: 20px;">
+    <a href="{{ route('agregarProveedor') }}" class="btn btn-primary" style="background-color: #7fff7dc5; color: rgb(0, 0, 0); border: none; padding: 10px 20px; border-radius: 5px;">
+        Agregar
+    </a>
+</div>
+
+<div class="proveedores-container">
+    @foreach($proveedores as $p)
+        <div class="proveedor-card">
+            <x-proveedor 
+                nombre="{{ $p->nombre }}" 
+                telefono="{{ $p->numTelefono }}" 
+                correo="{{ $p->correo }}" 
+                productos="{{ $p->tiposProducto }}" 
+                condicionesPago="{{ $p->condicionesPago }}" 
+                frecuenciaSuministro="{{ $p->frecuenciaSuministro }}"
+                horarioAtencion="{{ $p->horarioAtencion }}"
+                pais="{{ $p->pais }}"
+                ciudad="{{ $p->ciudad }}"
+                id="{{ $p->proveedorID }}"
+            >
+            </x-proveedor>
+        </div>
+    @endforeach
+</div>
+
+@endsection
+
 
 <script>
     function confirmarEliminacion($id) {
@@ -19,38 +53,3 @@
         });
     }
 </script>
-
-
-@section('css')
-    @vite('resources/css/proveedores.css')
-@endsection
-
-@section('contenido')
-
-<div style="text-align: center; margin: 20px;">
-    <a href="{{ route('agregarProveedor') }}" class="btn btn-primary" style="background-color: #7fff7dc5; color: rgb(0, 0, 0); border: none; padding: 10px 20px; border-radius: 5px;">
-        Agregar
-    </a>
-</div>
-
-
-@foreach($proveedores as $p)
-    
-
-<x-proveedor 
-nombre="{{ $p->nombre }}" 
-telefono="{{ $p->numTelefono }}" 
-correo="{{ $p->correo }}" 
-productos="{{ $p->tiposProducto }}" 
-condicionesPago="{{ $p->condicionesPago }}" 
-frecuenciaSuministro="{{ $p->frecuenciaSuministro }}"
-horarioAtencion="{{ $p->horarioAtencion }}"
-pais="{{ $p->pais }}"
-ciudad="{{ $p->ciudad }}"
-id="{{ $p->proveedorID }}"
->
-</x-proveedor>
-@endforeach
-
-
-@endsection
